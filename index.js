6a8de56d730c5fe34bca268311c020d1d0ddce9f
Dinar Davis <dinardavis@gmail.com>
2024-04-18 18:12:20 +0700

    update build files

--- index.js
+++ index.js
@@ -58,16 +58,28 @@ app.use("/posts", postRoutes);
 app.use(express.static(path.join(__dirname, "/client/build")))
 
 
-  /* ADVISORY WIDGET SEARCH */
-
-  app.get('/advisory', (req, res) => {
-    fetch(`https://www.travel-advisory.info/api`)
-    .then(res => res.json())
-    .then(data => res.json(data.data))
-      .catch((err) => {
-        console.log(err);
-    })
+/* ADVISORY WIDGET DATA SEARCH */
+app.get('/advisory', (req, res) => {
+  fetch(`https://www.travel-advisory.info/api`)
+  .then(res => res.json())
+  .then(data => res.json(data.data))
+    .catch((err) => {
+      console.log(err);
   })
+})
+
+
+const PHOTOS_API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY
+
+/* FLIGHT WIDGET DATA SEARCH */
+app.get('/flight', (req, res) => {
+  fetch(`https://api.unsplash.com/search/photos/?query=tokyo&client_id=${PHOTOS_API_KEY}&orientation=portrait&per_page=50`)
+      .then(res => res.json())
+      .then(data => res.json(data.results))
+      .catch((error) => {
+        console.log(error)
+      });
+})
 
 /* RENDER CLIENT FOR ALL PAGES */
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/client/build/index.html"))); 
