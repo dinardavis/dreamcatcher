bc163cbde2424c1364f9f948add86981dde9f0a5
Dinar Davis <dinardavis@gmail.com>
2024-06-27 11:46:34 -0700

    update project files

--- index.js
+++ index.js
@@ -164,8 +164,8 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
+    User.insertMany(users);
+    Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
 
